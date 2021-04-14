513ccd9fc5b0253102b39ed14b4d2d390825313d
Harsha Vardhan <gvsharsha6316@gmail.com>
2021-04-14 13:40:50 +0530

    docker

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+X
+(src/main/java/config/PropertyConfig.java,2\2\22ca2be42417dd14f16ca701b5413eba2f4d494c
+Q
+!src/test/java/tests/BaseTest.java,f\7\f76255b3e7b0a3a09fc110123ff89e0e8e0bb5c4
+U
+%src/main/java/pages/FlipKartPage.java,4\e\4e8bd826ddd345a460f87bd21bcf50f63718f3ad
+V
+&src/test/java/tests/flipkartLogin.java,9\6\963f61e20bce2248ed4edf0c659997accb1e2a34
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,10 +3,13 @@ package tests;
 import config.PropertyConfig;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
@@ -14,8 +17,17 @@ public class BaseTest {
 
     @BeforeMethod
     public void setUp() throws Exception {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+//        WebDriverManager.chromedriver().setup();
+////        driver = new ChromeDriver();
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+//        desiredCapabilities.setBrowserName(BrowserType.CHROME);
+//        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), desiredCapabilities);
+
+        WebDriverManager.firefoxdriver().setup();
+//        driver = new ChromeDriver();
+        desiredCapabilities.setBrowserName(BrowserType.FIREFOX);
+        driver = new RemoteWebDriver(new URL("http://localhost:4445/wd/hub"), desiredCapabilities);
+
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Long.parseLong(PropertyConfig.getPropValue("implicitTime")), TimeUnit.SECONDS);
     }
